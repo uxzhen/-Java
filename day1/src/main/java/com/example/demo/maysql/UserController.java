7504dd85804c1862d661b1938c5a9ef08c75e244
2554463433@qq.com <2554463433@qq.com>
2020-05-24 15:07:54 +0800

    对数据库添加操作（接收数据）

--- day1/src/main/java/com/example/demo/maysql/UserController.java
+++ day1/src/main/java/com/example/demo/maysql/UserController.java
@@ -3,6 +3,8 @@ package com.example.demo.maysql;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -10,12 +12,34 @@ import java.util.List;
 @RestController
 public class UserController {
     @Autowired
-    UserDao userDao ;
+    UserDao userDao;
 
     @GetMapping("/getAll")
-    public List getAll(){
+    public List getAll() {
         List all = userDao.findAll();
 
-        return  all;
+        return all;
+    }
+
+    @GetMapping("/add")
+    public User add() {
+        User user = new User();
+        user.setName("xudazhen");
+        user.setAge(18);
+        user.setAddress("sadsdasd");
+        User save = userDao.save(user);
+        return save;
+    }
+
+
+    //    127.0.0.1:8080/save?name=xasdasd&age=123&address=1231
+    @PostMapping("/save")
+    public String add(@RequestParam(name = "name") String name, @RequestParam(name = "address") String address, @RequestParam(name = "age") int age) {
+        User user = new User();
+        user.setName(name);
+        user.setAge(age);
+        user.setAddress(address);
+        userDao.save(user);
+        return "ok";
     }
 }
