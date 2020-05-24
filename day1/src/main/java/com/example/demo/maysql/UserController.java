23d071235e18f0a581e2a8145dc28daf86654849
2554463433@qq.com <2554463433@qq.com>
2020-05-24 17:02:18 +0800

    到这个地方数据库增删改查搞定

--- day1/src/main/java/com/example/demo/maysql/UserController.java
+++ day1/src/main/java/com/example/demo/maysql/UserController.java
@@ -2,10 +2,11 @@ package com.example.demo.maysql;
 
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+//import org.springframework.web.bind.annotation.GetMapping;
+//import org.springframework.web.bind.annotation.PostMapping;
+//import org.springframework.web.bind.annotation.RequestParam;
+//import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -32,9 +33,9 @@ public class UserController {
     }
 
 
-    //    127.0.0.1:8080/save?name=xasdasd&age=123&address=1231
+    //  添加数据库  127.0.0.1:8080/save?name=xasdasd&age=123&address=1231
     @PostMapping("/save")
-    public String add(@RequestParam(name = "name") String name, @RequestParam(name = "address") String address, @RequestParam(name = "age") int age) {
+    public String save(@RequestParam(name = "name") String name, @RequestParam(name = "address") String address, @RequestParam(name = "age") int age) {
         User user = new User();
         user.setName(name);
         user.setAge(age);
@@ -42,4 +43,34 @@ public class UserController {
         userDao.save(user);
         return "ok";
     }
+
+    // 更新数据库  127.0.0.1:8080/upData?id=3&name=a333&age=10&address
+    @PostMapping("/upData")
+    public String upData(@RequestParam(name = "name") String name, @RequestParam(name = "address") String address, @RequestParam(name = "age") int age, @RequestParam(name = "id") int id) {
+        User user = new User();
+        user.setName(name);
+        user.setId(id);
+        user.setAge(age);
+        user.setAddress(address);
+        userDao.save(user);
+        return "ok";
+    }
+    @PostMapping("/delAll")
+    public String deleteAll() {
+
+        userDao.deleteAll();
+        return "删除成功";
+    }
+
+    //    删除单个数据
+    @PostMapping("/delOne")
+    public String delOne(@RequestParam("id") int id) {
+
+        userDao.deleteById(id);
+        return "删除成功";
+    }
+
+
+
+
 }
