22a6f83070bf8ebcd121afaa44bf068884440f1e
uxzhen <2554463433@qq.com>
2020-05-25 11:42:26 +0800

    中午了

--- day1/src/main/java/com/example/demo/usser2/User2Controller.java
+++ day1/src/main/java/com/example/demo/usser2/User2Controller.java
@@ -2,6 +2,7 @@ package com.example.demo.usser2;
 
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,4 +18,6 @@ public class User2Controller {
         List all = user2Dao.findAll();
         return all;
     }
+
+
 }
--- day1/src/main/java/com/example/demo/usser2/User2Dao.java
+++ day1/src/main/java/com/example/demo/usser2/User2Dao.java
@@ -1,8 +1,12 @@
 package com.example.demo.usser2;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 // 此处的User2 对应表名
 public interface User2Dao extends JpaRepository<User2,Integer> {
+
 
 }
